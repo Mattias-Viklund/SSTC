6e9e21936d9be00256f4575ee0ff31c870b433c4
Mattias Wiklund <Sewq90@users.noreply.github.com>
2018-10-10 21:54:46 +0200

    a

---
+++ SSTC/SSTC/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSTC
+{
+    /// <summary>
+    /// SSTC - Split Secure Transfer Protocol
+    /// Proof of concept
+    /// </summary>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+    /// <summary>
+    /// Server - Placeholder until we can have a server thing
+    /// splits data to nodes
+    /// </summary>
+    class Server
+    {
+
+    }
+
+    class Splitter
+    {
+
+    }
+
+    /// <summary>
+    /// Node - Node to send data to client
+    /// </summary>
+    class Node
+    {
+
+    }
+}
